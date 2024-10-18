aefda8f90fee165e07981996aea29e66c8285dd9
Linus Silfver <linus.silfver@gmail.com>
2024-10-18 10:40:41 +0200

    made unit test for ReviewService caches

--- src/main/java/com/pubfinder/pubfinder/service/ReviewService.java
+++ src/main/java/com/pubfinder/pubfinder/service/ReviewService.java
@@ -14,7 +14,10 @@ import com.pubfinder.pubfinder.models.User;
 import com.pubfinder.pubfinder.models.enums.Volume;
 import org.apache.coyote.BadRequestException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
+import org.springframework.cache.annotation.Caching;
+import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
@@ -29,7 +32,8 @@ import java.util.function.ToIntFunction;
  */
 @Service
 public class ReviewService {
-
+    @Autowired
+    private ApplicationContext applicationContext;
     @Autowired
     private ReviewRepository reviewRepository;
 
@@ -45,17 +49,14 @@ public class ReviewService {
      * @param review the review
      * @return the review dto
      */
+    @Caching(evict = {@CacheEvict(value = "getPubRating", key = "#review.pubId"), @CacheEvict(value = "getPubReviews", key = "#review.pubId"), @CacheEvict(value = "getUserReviews", key = "#review.userId")})
     public ReviewResponseDto save(ReviewRequestDto review) {
 
-        User user = userRepository.findById(review.getUserId())
-                .orElseGet(() -> userRepository.save(User.builder().id(review.getUserId()).username(review.getUsername()).build()));
+        User user = userRepository.findById(review.getUserId()).orElseGet(() -> userRepository.save(User.builder().id(review.getUserId()).username(review.getUsername()).build()));
 
-        Pub pub = pubRepository.findById(review.getPubId())
-                .orElseGet(() -> pubRepository.save(Pub.builder().id(review.getPubId()).build()));
+        Pub pub = pubRepository.findById(review.getPubId()).orElseGet(() -> pubRepository.save(Pub.builder().id(review.getPubId()).build()));
 
-        Review savedReview = reviewRepository.findByPubAndReviewer(pub, user)
-                .map(existingReview -> updateOldReview(existingReview, review))
-                .orElseGet(() -> createNewReview(user, pub, review));
+        Review savedReview = reviewRepository.findByPubAndReviewer(pub, user).map(existingReview -> updateOldReview(existingReview, review)).orElseGet(() -> createNewReview(user, pub, review));
 
         return Mapper.INSTANCE.entityToDto(savedReview);
     }
@@ -67,51 +68,41 @@ public class ReviewService {
      * @throws ResourceNotFoundException the review not found exception
      */
     public void delete(UUID id) throws ResourceNotFoundException, BadRequestException {
-        Review review = reviewRepository.findById(id)
-                .orElseThrow(() -> new ResourceNotFoundException("Review: " + id + " not found."));
+        Review review = reviewRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Review: " + id + " not found."));
+        // this is split up into a separate function so the caching will work
+        applicationContext.getBean(ReviewService.class).deleteWithCacheEviction(review);
+    }
+
+    @Caching(evict = {@CacheEvict(value = "getPubRating", key = "#review.pub.id"), @CacheEvict(value = "getPubReviews", key = "#review.pub.id"), @CacheEvict(value = "getUserReviews", key = "#review.reviewer.id")})
+    public void deleteWithCacheEviction(Review review) {
         reviewRepository.delete(review);
     }
 
     @Cacheable(value = "getPubRating")
-    public RatingDto getPubRating(UUID id)
-            throws BadRequestException, ResourceNotFoundException {
+    public RatingDto getPubRating(UUID id) throws BadRequestException, ResourceNotFoundException {
         if (id == null) throw new BadRequestException();
 
-        Pub pub = pubRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Pub with id: " +id+ " was not found"));
+        Pub pub = pubRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Pub with id: " + id + " was not found"));
         List<Review> reviews = reviewRepository.findAllByPub(pub);
 
-        return RatingDto.builder()
-                .pubId(id)
-                .rating(calculateAverageRating(reviews, Review::getRating))
-                .toiletRating(calculateAverageRating(reviews, Review::getToilets))
-                .serviceRating(calculateAverageRating(reviews, Review::getService))
-                .volume(calculateAverageVolume(reviews))
-                .build();
+        return RatingDto.builder().pubId(id).rating(calculateAverageRating(reviews, Review::getRating)).toiletRating(calculateAverageRating(reviews, Review::getToilets)).serviceRating(calculateAverageRating(reviews, Review::getService)).volume(calculateAverageVolume(reviews)).build();
     }
 
     @Cacheable(value = "getPubReviews")
-    public List<ReviewResponseDto> getPubReviews(UUID id)
-            throws BadRequestException {
+    public List<ReviewResponseDto> getPubReviews(UUID id) throws BadRequestException {
         if (id == null) throw new BadRequestException();
 
         Optional<Pub> pub = pubRepository.findById(id);
-        return pub.map(value -> reviewRepository.findAllByPub(value)
-                .stream()
-                .map(Mapper.INSTANCE::entityToDto)
-                .toList()).orElseGet(List::of);
+        return pub.map(value -> reviewRepository.findAllByPub(value).stream().map(Mapper.INSTANCE::entityToDto).toList()).orElseGet(List::of);
 
     }
 
     @Cacheable(value = "getUserReviews")
-    public List<ReviewResponseDto> getUserReviews(UUID id)
-            throws ResourceNotFoundException, BadRequestException {
+    public List<ReviewResponseDto> getUserReviews(UUID id) throws BadRequestException {
         if (id == null) throw new BadRequestException();
 
         Optional<User> user = userRepository.findById(id);
-        return user.map(value -> reviewRepository.findAllByReviewer(value)
-                .stream()
-                .map(Mapper.INSTANCE::entityToDto)
-                .toList()).orElseGet(List::of);
+        return user.map(value -> reviewRepository.findAllByReviewer(value).stream().map(Mapper.INSTANCE::entityToDto).toList()).orElseGet(List::of);
 
     }
 
@@ -128,30 +119,17 @@ public class ReviewService {
     }
 
     private Review createNewReview(User user, Pub pub, ReviewRequestDto reviewRequestDto) {
-        return reviewRepository.save(Review.builder()
-                .reviewer(user)
-                .pub(pub)
-                .rating(reviewRequestDto.getRating())
-                .service(reviewRequestDto.getService())
-                .toilets(reviewRequestDto.getToilets())
-                .review(reviewRequestDto.getReview())
-                .volume(Volume.fromValue((reviewRequestDto.getVolume())))
-                .reviewDate(LocalDateTime.now())
-                .build());
+        return reviewRepository.save(Review.builder().reviewer(user).pub(pub).rating(reviewRequestDto.getRating()).service(reviewRequestDto.getService()).toilets(reviewRequestDto.getToilets()).review(reviewRequestDto.getReview()).volume(Volume.fromValue((reviewRequestDto.getVolume()))).reviewDate(LocalDateTime.now()).build());
     }
 
     private int calculateAverageRating(List<Review> reviews, ToIntFunction<Review> extractor) {
-        int[] ratings = reviews.stream()
-                .mapToInt(extractor)
-                .filter(rating -> rating != 0)
-                .toArray();
+        int[] ratings = reviews.stream().mapToInt(extractor).filter(rating -> rating != 0).toArray();
 
         return calculateAverage(Arrays.stream(ratings).sum(), ratings.length);
     }
 
     private Volume calculateAverageVolume(List<Review> reviews) {
-        int[] loudness = reviews.stream().filter(r -> r.getVolume() != null)
-                .mapToInt(r -> r.getVolume().getOrdinal()).toArray();
+        int[] loudness = reviews.stream().filter(r -> r.getVolume() != null).mapToInt(r -> r.getVolume().getOrdinal()).toArray();
         if (loudness.length == 0) return null;
 
         int value = calculateAverage(Arrays.stream(loudness).sum(), loudness.length);
